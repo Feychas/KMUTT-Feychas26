5fd5a47c61b5dea47080aff882a04726fdda9cb8
Felice Christiara Median Putri <felice.putri@mail.kmutt.ac.th>
2025-09-16 23:13:26 +0700

    Update no 1 edian.java

--- CSCMS /no 1 median.java
+++ CSCMS /no 1 median.java
@@ -1,1 +1,14 @@
-something
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        Scanner xx = new Scanner(System.in);
+        
+        int a = xx.nextInt();
+        int b = xx.nextInt();
+        int c = xx.nextInt();
+        
+        int res = a + b + c - Math.max(a, Math.max(b, c)) - Math.min(a, Math.min(b, c));
+        System.out.print(res);
+        
+    }
+}
