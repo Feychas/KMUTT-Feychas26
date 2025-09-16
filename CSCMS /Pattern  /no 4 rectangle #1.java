fdaa9e2e257d2f253da414f3e366a8250e6dd580
Felice Christiara Median Putri <felice.putri@mail.kmutt.ac.th>
2025-09-16 13:53:00 +0700

    Update no 4 rectangle #1.java

--- CSCMS /Pattern  /no 4 rectangle #1.java
+++ CSCMS /Pattern  /no 4 rectangle #1.java
@@ -1,1 +1,13 @@
-wait
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+  	Scanner xx = new Scanner(System.in);
+    
+     int inp = xx.nextInt();
+    
+     for (int i = 1 ; i <= inp ; i++) { //outter for row ke bawah 
+     	 for (int j = 1 ; j <= inp ; j++) { //innner for coloumn, how many kesamping
+          System.out.print("*");
+     } System.out.println(); }
+  }
+}
