9d720bc31b446dc46a24ff5b89ab5e15eb1372c7
Felice Christiara Median Putri <felice.putri@mail.kmutt.ac.th>
2025-09-19 16:15:27 +0700

    Create no 12 Hello Username.java

---
+++ CSCMS /no 12 Hello Username.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+      
+        String x = s.nextLine();
+        System.out.print("Hello, " + x + ".");
+        
+    }
+}
