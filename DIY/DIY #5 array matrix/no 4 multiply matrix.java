1e5c019afeadf51a071b4503014c07c3193d5ab3
Felice Christiara Median Putri <felice.putri@mail.kmutt.ac.th>
2025-10-02 15:20:41 +0700

    Create  no 4 multiply matrix.java

---
+++ DIY/DIY #5 array matrix/no 4 multiply matrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Maxixi {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        
+        double[][] m1 = new double[3][3];
+        double[][] m2 = new double[3][3];
+        double[][] res = new double[3][3];
+        
+        for (int i = 0 ; i < 3 ; i++) {
+            for (int j = 0 ; j < 3 ; j++) {
+                m1[i][j] = x.nextDouble();
+            }
+        }
+        for (int i = 0 ; i < 3 ; i++) {
+            for (int j = 0 ; j < 3 ; j++) {
+                m2[i][j] = x.nextDouble();
+            }
+        }
+        //batas input
+        for (int i = 0 ; i < 3 ; i++) {
+            for (int j = 0 ; j < 3 ; j++) {
+                for (int k = 0 ; k < 3 ; k++) {
+                    res[i][j] += m1[i][k] * m2[k][j]; //logika aja dr gambar, intinya 3 variabel loop
+                } System.out.print(res[i][j] + " ");
+            } System.out.println();
+        } //langsung print, mff malas
+    }
+}
