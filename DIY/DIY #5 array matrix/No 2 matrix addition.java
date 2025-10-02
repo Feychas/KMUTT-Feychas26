9fa0f7910ad6998c1ebadd132274905ad44ff941
Felice Christiara Median Putri <felice.putri@mail.kmutt.ac.th>
2025-10-02 15:30:01 +0700

    Create No 2 matrix addition.java

---
+++ DIY/DIY #5 array matrix/No 2 matrix addition.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class AdditionMatrix {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        double[][] m1 = new double[2][2];
+        double[][] m2 = new double[2][2];
+        double[][] res = new double[2][2];
+        
+        for (int i = 0 ; i < 2 ; i++) {
+            for (int j = 0 ; j < 2 ; j++) {
+                m1[i][j] = x.nextDouble();
+            }
+        }
+        for (int i = 0 ; i < 2 ; i++) {
+            for (int j = 0 ; j < 2 ; j++) {
+                m2[i][j] = x.nextDouble();
+            }
+        }
+        //batas input
+        for (int i = 0 ; i < 2 ; i++) {
+            for (int j = 0 ; j < 2 ; j++) {
+                res[i][j] = m1[i][j] + m2[i][j];
+                System.out.print(res[i][j] + " ");
+            } System.out.println();
+        } 
+    }
+}
