02624f4971a4baab7d4798f71778e139f0ad8807
Felice Christiara Median Putri <felice.putri@mail.kmutt.ac.th>
2025-10-02 16:04:53 +0700

    Create No 6 bomb 3D.java

---
+++ DIY/DIY #5 array matrix/No 6 bomb 3D.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class RubikBomb {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int nbomb = s.nextInt();
+        boolean[][][] rub = new boolean[5][5][5];
+        
+        for (int l = 0 ; l < nbomb ; l++) {
+            int x = s.nextInt();
+            int y = s.nextInt();
+            int z = s.nextInt();
+            rub[x][y][z] = true; //tandain kalau dia dor
+            
+            for (int i = 0 ; i < 5 ; i++) { //dor x axis ats bwh vertikal
+                rub[i][y][z] = true;
+            }
+            for (int j = 0 ; j < 5 ; j++) { //dor y axis knn kri horizontal
+                rub[x][j][z] = true;
+            }
+            for (int k = 0 ; k < 5 ; k++) { //dor z axis dpn blkng horizontal
+                rub[x][y][k] = true;
+            }
+        }
+      
+        int count = 0;
+        for (int i = 0 ; i < 5 ; i++) {
+            for (int j = 0 ; j < 5 ; j++) {
+                for (int k = 0 ; k < 5 ; k++) {
+                    if (rub[i][j][k]) { //if it's true
+                        count++;
+                    }
+        }   }   } 
+        System.out.print(count);
+    }
+}
