d08376031500ba64a09a1dec41a9d0dece6ccda5
Felice Christiara Median Putri <felice.putri@mail.kmutt.ac.th>
2025-10-17 16:16:21 +0700

    Update No 2 matrix addition.java

--- DIY/DIY #5 array matrix/No 2 matrix addition.java
+++ DIY/DIY #5 array matrix/No 2 matrix addition.java
@@ -25,3 +25,35 @@ public class AdditionMatrix {
         } 
     }
 }
+
+/* SAME THO BUT NO NEW RESSULT ARRAY
+import java.util.Scanner;
+public class MatrixPlus {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        double[][] m1 = new double[2][2];
+        double[][] m2 = new double[2][2];
+        
+        for (int i = 0 ; i < 2 ; i++) {
+            for (int j = 0 ; j < 2 ; j++) {
+                m1[i][j] = x.nextDouble();
+            }
+        }
+        
+        for (int i = 0 ; i < 2 ; i++) {
+            for (int j = 0 ; j < 2 ; j++) {
+                m2[i][j] = x.nextDouble();
+            }
+        }
+        
+        for (int i = 0 ; i < 2 ; i++) {
+            for (int j = 0 ; j < 2 ; j++) {
+                m1[i][j] += m2[i][j];
+                System.out.print(m1[i][j] + " ");
+            } System.out.println();
+        }
+        
+    }
+}
+
+*/
